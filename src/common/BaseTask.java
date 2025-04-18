de52da037e5f507551577cccfc88f21080ea129a
shengnan <pelevin1111@gmail.com>
2025-04-18 23:05:37 +0300

    [UPD] add new method showMenu() & implemented method saveToExcel()

--- src/common/BaseTask.java
+++ src/common/BaseTask.java
@@ -1,28 +1,36 @@
 package common;
 
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
 
 public abstract class BaseTask {
     protected SQLTools sqlTools;
+    protected static String menuText = "";
 
     public BaseTask(SQLTools sqlTools) throws SQLException {
         this.sqlTools = sqlTools;
+    }
+
+    public void showMenu(String menuText) {
+        System.out.println("📋 Меню:");
+        System.out.println(menuText);
     }
 
     public void showTables() throws SQLException {
         this.sqlTools.showTables();
     }
 
-    public void createTables(List<String> tableNames, List<Map<String, String>> columnsList) throws SQLException {
-        this.sqlTools.createTables(tableNames, columnsList);
+    public void createTables() throws SQLException {
+        this.sqlTools.createTables();
     }
 
     public void insertRowIntoDB(String tablename, Map<String, Object> data) throws SQLException {
         this.sqlTools.insertRowIntoDB(tablename, data);
     }
 
-    public abstract void saveToExcel();
+    public void saveToExcel() {
+        this.sqlTools.saveToExcel();
+    }
+
 
 }
