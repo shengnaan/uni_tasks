c48ec9c980b4fe2bedecd9374a5caa0799309fd4
Marina <marinall11xx@gmail.com>
2025-04-22 01:04:09 +0300

    [UPD] new: db schemas now

---
+++ src/tasks/task_3/Main.java
@@ -0,0 +1,66 @@
+package tasks.task_3;
+
+import common.BaseTask;
+import common.SQLTools;
+
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main extends BaseTask {
+
+    public Main(String dbName, Map<String, Map<String, String>> tableSchemas) throws SQLException {
+        super(new SQLTools(dbName, tableSchemas));
+        menuText = """
+                1. Вывести все таблицы из БД.
+                2. Создать таблицу(-ы) в БД.
+                3. Проверить числа на целостность и чётность
+                4. Сохранить все данные (вышеполученные результаты) из БД в Excel и вывести на экран.
+                5. Показать меню
+                6. Остановить программу.
+                """;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        final Map<String, Map<String, String>> tableSchemas = Map.of(
+                "number_checks", Map.of(
+                        "id", "SERIAL PRIMARY KEY",
+                        "number", "VARCHAR(50)",
+                        "is_integer", "BOOLEAN",
+                        "is_even", "BOOLEAN"
+                )
+        );
+        final String dbName = "task_3";
+        tasks.task_3.Main main = new tasks.task_3.Main(dbName, tableSchemas);
+
+        System.out.println("Практическая работа 3");
+        main.showMenu(menuText);
+
+        String var = "";
+        int menuPunkt = 0;
+
+        Scanner sc = new Scanner(System.in);
+        while (!"12".equals(var)) {
+            var = sc.next();
+
+            try {
+                menuPunkt = Integer.parseInt(var);
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный формат ввода!");
+            }
+
+            switch (menuPunkt) {
+                case 1 -> main.showTables();
+                case 2 -> main.createTables();
+                case 3 -> main.checkNumbers();
+                case 4 -> main.saveToExcel();
+                case 5 -> main.showMenu(menuText);
+                case 6 -> System.out.println("Программа завершила работу");
+                default -> System.out.println("Неверная опция");
+            }
+        }
+        main.sqlTools.closeConnection();
+    }
+
+
+}
