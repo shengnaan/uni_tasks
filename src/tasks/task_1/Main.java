91f1e3f868f439805c5ae1532db10421cc2d8b46
shengnan <pelevin1111@gmail.com>
2025-04-21 17:32:24 +0300

    [FIX] add return when NumberFormatException in menu

--- src/tasks/task_1/Main.java
+++ src/tasks/task_1/Main.java
@@ -53,6 +53,7 @@ public class Main extends BaseTask {
                 menuPunkt = Integer.parseInt(var);
             } catch (NumberFormatException e) {
                 System.out.println("Неверный формат ввода!");
+                return;
             }
 
             switch (menuPunkt) {
--- src/tasks/task_5/Main.java
+++ src/tasks/task_5/Main.java
@@ -48,6 +48,7 @@ public class Main extends BaseTask {
                 menuPunkt = Integer.parseInt(var);
             } catch (NumberFormatException e) {
                 System.out.println("Неверный формат ввода!");
+                return;
             }
 
             switch (menuPunkt) {
